31dfa90219cb2fffc33ad34f5d3e613417d65fff
TheRealDwright <dan@iamaman.com.au>
2016-02-12 10:57:55 +1000

    Update  Backup Script
    AutoBackup Script

--- ec2Backup.py
+++ ec2Backup.py
@@ -3,31 +3,31 @@ import boto3
 ec = boto3.client('ec2')
 
 
-def lambda_handler(event, context):
-    reservations = ec.describe_instances(
-        Filters=[
-            {'Name': 'tag-key', 'Values': ['backup', 'Backup']},
-        ]
-    ).get(
-        'Reservations', []
-    )
+reservations = ec.describe_instances(
+    #Filter all instances with the key:pair of AutoBackup:True
+    Filters=[
+        {'Name':'tag:AutoBackup', 'Values':['True']},
+    ]
+).get(
+    'Reservations', []
+)
 
-    instances = sum(
-        [
-            [i for i in r['Instances']]
-            for r in reservations
-        ], [])
+#Locate Instances to be backed up.
+instances = sum(
+    [
+        [i for i in r['Instances']]
+        for r in reservations
+    ], [])
+print "Found %d instances that need backing up" % len(instances)
 
-    print "Found %d instances that need backing up" % len(instances)
-
-    for instance in instances:
-        for dev in instance['BlockDeviceMappings']:
-            if dev.get('Ebs', None) is None:
-                continue
-            vol_id = dev['Ebs']['VolumeId']
-            print "Found EBS volume %s on instance %s" % (
-                vol_id, instance['InstanceId'])
-
-            ec.create_snapshot(
-                VolumeId=vol_id,
-            )
+for instance in instances:
+    for dev in instance['BlockDeviceMappings']:
+        if dev.get('Ebs', None) is None:
+            # skip anything that does not have EBS volumes
+            continue
+        vol_id = dev['Ebs']['VolumeId']
+        print "Found EBS volume %s on instance %s" % (
+            vol_id, instance['InstanceId'])
+        ec.create_snapshot(
+            VolumeId=vol_id,
+        )
