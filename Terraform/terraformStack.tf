392c14b0d6617015e35448f230de79c0cab29ca9
TheRealDwright <dan@iamaman.com.au>
2016-05-11 10:54:58 +1000

    Terraform WIP

---
+++ Terraform/terraformStack.tf
@@ -0,0 +1,93 @@
+#--------------------------------------------------------------
+# VPC
+#--------------------------------------------------------------
+resource "aws_vpc" "main" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_eip" "one" {
+  vpc                       = true
+}
+
+resource "aws_eip" "two" {
+  vpc                       = true
+}
+
+resource "aws_subnet" "public_1" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.public_subnet_cidr_1}"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.public_subnet_cidr_2}"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_1" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.private_subnet_cidr_1}"
+    map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_2" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.private_subnet_cidr_2}"
+    map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "db_1" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.db_subnet_cidr_1}"
+    map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "db_2" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.db_subnet_cidr_2}"
+    map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+}
+
+resource "aws_route_table" "private_1" {
+    vpc_id = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+}
+
+resource "aws_main_route_table_association" "public" {
+    vpc_id = "${aws_vpc.main.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+
+#--------------------------------------------------------------
+# Security Group
+#--------------------------------------------------------------
+resource "aws_security_group" "allow_all" {
+  name = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,61 @@
+#--------------------------------------------------------------
+# AWS settings
+#--------------------------------------------------------------
+variable "access_key" {
+  description = "AWS access key"
+}
+
+variable "secret_key" {
+  description = "AWS secret access key"
+}
+
+/* AWS VPC Subnet Variables */
+
+variable "region"     {
+  description = "AWS region to host your network"
+  default     = "us-west-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  default     = "10.120.0.0/16"
+}
+
+variable "public_subnet_cidr_1" {
+  description = "CIDR for public subnet"
+  default     = "10.120.1.0/24"
+}
+
+variable "public_subnet_cidr_2" {
+  description = "CIDR for public subnet"
+  default     = "10.120.2.0/24"
+}
+
+variable "private_subnet_cidr_1" {
+  description = "CIDR for private subnet"
+  default     = "10.120.4.0/22"
+}
+
+variable "private_subnet_cidr_2" {
+  description = "CIDR for private subnet"
+  default     = "10.120.8.0/22"
+}
+
+variable "db_subnet_cidr_1" {
+  description = "CIDR for db subnet"
+  default     = "10.120.200.0/22"
+}
+
+variable "db_subnet_cidr_2" {
+  description = "CIDR for db subnet"
+  default     = "10.120.210.0/22"
+}
+
+/* Ubuntu 14.04 amis by region */
+variable "amis" {
+  description = "Base AMI to launch the instances with"
+  default = {
+    us-west-1 = "ami-06116566"
+    us-west-2 = "ami-9abea4fb"
+  }
+}
