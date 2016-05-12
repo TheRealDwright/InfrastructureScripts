020a5816f5bd9d84f638171ff18b4fd981febaef
TheRealDwright <dan@iamaman.com.au>
2016-05-12 11:46:54 +1000

    Add autoscaling logic

--- Terraform/terraformStack.tf
+++ Terraform/terraformStack.tf
@@ -20,36 +20,42 @@ resource "aws_subnet" "public_1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "${var.public_subnet_cidr_1}"
     map_public_ip_on_launch = true
+    availability_zone = "${var.availability_zone_1}"
 }
 
 resource "aws_subnet" "public_2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "${var.public_subnet_cidr_2}"
     map_public_ip_on_launch = true
+    availability_zone = "${var.availability_zone_2}"
 }
 
 resource "aws_subnet" "private_1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "${var.private_subnet_cidr_1}"
     map_public_ip_on_launch = false
+    availability_zone = "${var.availability_zone_1}"
 }
 
 resource "aws_subnet" "private_2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "${var.private_subnet_cidr_2}"
     map_public_ip_on_launch = false
+    availability_zone = "${var.availability_zone_2}"
 }
 
 resource "aws_subnet" "db_1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "${var.db_subnet_cidr_1}"
     map_public_ip_on_launch = false
+    availability_zone = "${var.availability_zone_1}"
 }
 
 resource "aws_subnet" "db_2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "${var.db_subnet_cidr_2}"
     map_public_ip_on_launch = false
+    availability_zone = "${var.availability_zone_2}"
 }
 
 resource "aws_route_table" "public" {
@@ -118,9 +124,7 @@ resource "aws_nat_gateway" "2" {
     subnet_id = "${aws_subnet.public_2.id}"
   depends_on = ["aws_internet_gateway.gw",]
 }
-#--------------------------------------------------------------
-# Security Group
-#--------------------------------------------------------------
+
 resource "aws_security_group" "allow_all" {
   name = "allow_all"
   description = "Allow all inbound traffic"
@@ -132,4 +136,83 @@ resource "aws_security_group" "allow_all" {
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "atlas_artifact" "helloworld"{
+  name = "simplehq/helloworld"
+  type = "amazon.image"
+  version = "latest"
+}
+
+resource "aws_elb" "helloworld" {
+  name = "helloworld-elb"
+  subnets = ["${aws_subnet.public_1.id}", "${aws_subnet.public_2.id}"]
+  internal = false
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+  security_groups = ["${aws_security_group.allow_all.id}"]
+
+    listener {
+      instance_port = 8080
+      instance_protocol = "http"
+      lb_port = 80
+      lb_protocol = "http"
+    }
+
+    health_check {
+      healthy_threshold = 2
+      unhealthy_threshold = 2
+      timeout = 3
+      target = "HTTP:8080/"
+      interval = 30
+    }
+
+  tags {
+    Name = "helloworld-terraform-elb"
+  }
+}
+
+resource "aws_launch_configuration" "helloworld" {
+    name_prefix = "terraform-lc-${atlas_artifact.helloworld.name}"
+    image_id = "${atlas_artifact.helloworld.metadata_full.region-us-west-2}"
+    instance_type = "t2.micro"
+    key_name = "cameron-test"
+    security_groups = ["${aws_security_group.allow_all.id}"]
+
+    root_block_device {
+      volume_type = "gp2"
+      volume_size = "8"
+      delete_on_termination = "true"
+    }
+
+    lifecycle {
+      create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "helloworld" {
+    name = "helloworld-example"
+    launch_configuration = "${aws_launch_configuration.helloworld.name}"
+    availability_zones = ["${var.availability_zone_1}", "${var.availability_zone_2}"]
+    vpc_zone_identifier = ["${aws_subnet.private_1.id}", "${aws_subnet.private_2.id}"]
+    max_size = 5
+    min_size = 2
+    health_check_grace_period = 300
+    health_check_type = "ELB"
+    desired_capacity = 4
+    force_delete = true
+    load_balancers = ["${aws_elb.helloworld.name}"]
+
+    lifecycle {
+      create_before_destroy = true
+    }
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -4,53 +4,53 @@
 variable "access_key" {
   description = "AWS access key"
 }
-
 variable "secret_key" {
   description = "AWS secret access key"
 }
-
 /* AWS VPC Subnet Variables */
-
 variable "region"     {
   description = "AWS region to host your network"
-  default     = "us-west-1"
+  default     = "us-west-2"
+}
+
+variable "availability_zone_1" {
+  description = "AWS region to host your network"
+  default     = "us-west-2a"
+}
+
+variable "availability_zone_2" {
+  description = "AWS region to host your network"
+  default     = "us-west-2b"
 }
 
 variable "vpc_cidr" {
   description = "CIDR for VPC"
   default     = "10.120.0.0/16"
 }
-
 variable "public_subnet_cidr_1" {
   description = "CIDR for public subnet"
   default     = "10.120.1.0/24"
 }
-
 variable "public_subnet_cidr_2" {
   description = "CIDR for public subnet"
   default     = "10.120.2.0/24"
 }
-
 variable "private_subnet_cidr_1" {
   description = "CIDR for private subnet"
   default     = "10.120.4.0/22"
 }
-
 variable "private_subnet_cidr_2" {
   description = "CIDR for private subnet"
   default     = "10.120.8.0/22"
 }
-
 variable "db_subnet_cidr_1" {
   description = "CIDR for db subnet"
   default     = "10.120.200.0/22"
 }
-
 variable "db_subnet_cidr_2" {
   description = "CIDR for db subnet"
   default     = "10.120.210.0/22"
 }
-
 /* Ubuntu 14.04 amis by region */
 variable "amis" {
   description = "Base AMI to launch the instances with"
